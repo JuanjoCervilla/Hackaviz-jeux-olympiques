a996610176387aad1ed599cc9623a5b62fa7f7b6
Juanjo <jj.cergoma@gmail.com>
2024-03-11 11:10:54 +0100

    semi-control-side done

--- aplication.py
+++ aplication.py
@@ -21,124 +21,7 @@ from app import app
 ################################ LAYOUT ######################################
 ##############################################################################
 
-############################### FIRST CARD ###################################
-
-first_card = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                ################## FIRST ROW ##################
-                dbc.Row(
-                    [
-                        #Having more space on the left side
-                        dbc.Col(
-                            [],
-                            width=1,
-                        ),
-                        
-                        #RadioItems for type of Jeux
-                        dbc.Col(
-                            [
-                                dcc.RadioItems(
-                                    id='RadioItems_Jeux',
-                                    value = list(df_jo.Jeux.unique())[0],
-                                    options=[{'label': str(x), 'value': x} for x in df_jo.Jeux.unique()],
-                                    # inline=True,
-                                    labelStyle={'display': 'inline-block',
-                                                'background' : '#66B2FF',
-                                                'padding': '0.4rem 1.1rem',
-                                                'border-radius':'0.3rem',
-                                                'margin' : '0.7rem',
-                                                },
-                                ),
-                            ],
-                            width=3,
-                        ),                        
-                        
-                        #Dropdown for Discipline
-                        dbc.Col(
-                            [
-                                #html.Label('Discipline', style = {'font-weight': 'bold', 'font-size': 16}),
-                                dcc.Dropdown(
-                                    id = 'Dropdown_Discipline' ,
-                                    options= df_jo.Discipline.unique(),
-                                    #value = ['Jan'],
-                                    placeholder= 'Sélectionnez une discipline',
-                                    multi= False,
-                                    clearable=False
-                                ),
-                            ],
-                            width=3,
-                            className="text-center",
-                        ),
-                        
-                        dbc.Col(
-                            [
-                            html.Br(),
-                            html.P("Select Check-In Time"),
-                            dcc.DatePickerRange(
-                                id="date-picker-select",
-                                start_date=dt(2024, 8, 1),
-                                end_date=dt(2024, 8, 5),
-                                min_date_allowed=dt(2024, 8, 1),
-                                max_date_allowed=dt(2024, 8, 31),
-                                initial_visible_month=dt(2024, 8, 1),
-                            ),
-                            ]
-                        )
-                        
-                    ]
-                ),
-                                
-                 ################## SECOND ROW ##################
-                dbc.Row(
-            [
-                # Having more space between dropdowns
-                dbc.Col(
-                    [],
-                    width=1,
-                ),
-                
-                #RadioItems for Genre
-                dbc.Col(
-                [
-                dcc.RadioItems(
-                            id='RadioItems_Genre',
-                            value='Hommes',
-                            options= [{'label': 'Hommes', 'value': 'Hommes'}, {'label': 'Femmes', 'value': 'Femmes'},{'label': 'Mixte', 'value': 'Mixte'}],#[{'label': x, 'value': x} for x in df_jo.Genre.unique()],
-                            labelStyle={'display': 'inline-block',
-                                        'background' : '#CCE5FF',
-                                        'padding': '0.3rem 0.5rem',
-                                        'border-radius':'0.3rem',
-                                        'margin' : '0.5rem',
-                                        },
-                            style={'font-size': 15}
-                            ),
-                ],
-                    width=3,
-                ),
-                
-                # Dropdown for épreuve                
-                dbc.Col(
-                            [
-                                #html.Label('Épreuve', style = {'font-weight': 'bold', 'font-size': 16}),
-                                dcc.Dropdown(
-                                    id='Dropdown_Epreuve',
-                                    options= df_jo.Épreuve.unique(),
-                                    clearable=False,
-                                    placeholder= 'Sélectionnez une épreuve',
-                                    multi=False,
-                                ),
-                            ],
-                            width=3,
-                            className="text-center",
-                        ),              
-            ],
-        ),     
-            ]
-        )
-    ]
-)
+############################### CONTROL SIDE ###################################
 
 header = dbc.Card([
     html.Div(
@@ -150,18 +33,84 @@ header = dbc.Card([
         ),
 ])
 
-app.layout = html.Div([
-    header,
-    first_card
-])
+def generate_control_card():
+    """
+    :return: A Div containing controls for graphs.
+    """
+    return html.Div(
+        id="control-card",
+        children=[
+            html.P("Sélectionnez le type de jeux"),
+            dcc.RadioItems(
+                id='RadioItems_Jeux',
+                value = list(df_jo.Jeux.unique())[0],
+                options=[{'label': str(x), 'value': x} for x in df_jo.Jeux.unique()],
+                # inline=True,
+                labelStyle={'display': 'inline-block',
+                            'background' : '#66B2FF',
+                            'padding': '0.4rem 1.1rem',
+                            'border-radius':'0.3rem',
+                            'margin' : '0.7rem',
+                            },
+            ),
+            html.Br(),
+            html.P("Sélectionnez le Genre de la discipline"),
+            dcc.RadioItems(
+                        id='RadioItems_Genre',
+                        value='Hommes',
+                        options= [{'label': 'Hommes', 'value': 'Hommes'}, {'label': 'Femmes', 'value': 'Femmes'},{'label': 'Mixte', 'value': 'Mixte'}],#[{'label': x, 'value': x} for x in df_jo.Genre.unique()],
+                        labelStyle={'display': 'inline-block',
+                                    'background' : '#CCE5FF',
+                                    'padding': '0.3rem 0.5rem',
+                                    'border-radius':'0.3rem',
+                                    'margin' : '0.5rem',
+                                    },
+                        style={'font-size': 15}
+                        ),
+            html.Br(),
+            html.P("Sélectionnez la discipline"),
+            dcc.Dropdown(
+                id = 'Dropdown_Discipline' ,
+                options= df_jo.Discipline.unique(),
+                placeholder= 'Sélectionnez une discipline',
+                multi= False,
+                clearable=False
+            ),
+            html.Br(),
+            html.P("Sélectionnez l'épreuve"),
+            dcc.Dropdown(
+                id='Dropdown_Epreuve',
+                options= df_jo.Épreuve.unique(),
+                clearable=False,
+                placeholder= 'Sélectionnez une épreuve',
+                multi=False,
+            ),
+            html.Br(),
+            html.P("Select Check-In Time"),
+            dcc.DatePickerRange(
+                id="date-picker-select",
+                display_format = "DD / MM / YY" ,
+            ),
+        ],
+    )
 
+app.layout = html.Div(
+    id="app-container",
+    children=[
+        header,
+        html.Div(
+            id="left-column",
+            className="four columns",
+            children=[generate_control_card()]
+        )
+    ]
+)
 
 ##############################################################################
 ############################## CALLBACK ######################################
 ##############################################################################
 
 #Callback for Disciplines
-
 @app.callback(
     Output("Dropdown_Discipline", "options"),
     Input("RadioItems_Jeux", "value"),
@@ -196,8 +145,7 @@ def values_dropdown_category(jeux, genre):
     return disciplines[0]
 
 
-#Callback for Disciplines
-
+#Callback for Epreuves
 @app.callback(
     Output("Dropdown_Epreuve", "options"),
     Input("RadioItems_Jeux", "value"),
@@ -235,6 +183,97 @@ def values_dropdown_category(jeux, genre, discipline):
 
     return epreuves[0]
 
+
+# #Callback for Start Date calendar
+@app.callback(
+    Output("date-picker-select", "start_date"),
+    Input("RadioItems_Jeux", "value"),
+    Input("RadioItems_Genre", "value"),
+    Input("Dropdown_Discipline", "value"),
+    Input("Dropdown_Epreuve", "value"),
+)
+def options_dropdown_category(jeux, genre, discipline, epreuve):
+
+    df = df_jo.copy()
+    df = df[df['Jeux'] == jeux]
+    df = df[df['Genre'] == genre]
+    df = df[df['Discipline'] == discipline]
+    df = df[df['Épreuve'] == epreuve]
+
+    return dt.strptime(df['Date_Début'].min(), '%m-%d-%Y') 
+
+#Callback for Max Start Date calendar
+@app.callback(
+    Output("date-picker-select", "min_date_allowed"),
+    Input("RadioItems_Jeux", "value"),
+    Input("RadioItems_Genre", "value"),
+    Input("Dropdown_Discipline", "value"),
+    Input("Dropdown_Epreuve", "value"),
+)
+def options_dropdown_category(jeux, genre, discipline, epreuve):
+
+    df = df_jo.copy()
+    df = df[df['Jeux'] == jeux]
+    df = df[df['Genre'] == genre]
+    df = df[df['Discipline'] == discipline]
+    df = df[df['Épreuve'] == epreuve]
+    
+    return dt.strptime(df['Date_Début'].min(), '%m-%d-%Y') 
+
+
+#Callback for End Date calendar
+@app.callback(
+    Output("date-picker-select", "end_date"),
+    Input("RadioItems_Jeux", "value"),
+    Input("RadioItems_Genre", "value"),
+    Input("Dropdown_Discipline", "value"),
+    Input("Dropdown_Epreuve", "value"),
+)
+def options_dropdown_category(jeux, genre, discipline, epreuve):
+
+    df = df_jo.copy()
+    df = df[df['Jeux'] == jeux]
+    df = df[df['Genre'] == genre]
+    df = df[df['Discipline'] == discipline]
+    df = df[df['Épreuve'] == epreuve]
+
+    return dt.strptime(df['Date_Début'].max(), '%m-%d-%Y')
+
+# #Callback for Max End Date calendar
+@app.callback(
+    Output("date-picker-select", "max_date_allowed"),
+    Input("RadioItems_Jeux", "value"),
+    Input("RadioItems_Genre", "value"),
+    Input("Dropdown_Discipline", "value"),
+    Input("Dropdown_Epreuve", "value"),
+)
+def options_dropdown_category(jeux, genre, discipline, epreuve):
+
+    df = df_jo.copy()
+    df = df[df['Jeux'] == jeux]
+    df = df[df['Genre'] == genre]
+    df = df[df['Discipline'] == discipline]
+    df = df[df['Épreuve'] == epreuve]
+
+    return dt.strptime(df['Date_Début'].max(), '%m-%d-%Y')
+
+# #Callback for Initial_Visible_Month
+@app.callback(
+    Output("date-picker-select", "initial_visible_month"),
+    Input("RadioItems_Jeux", "value"),
+    Input("RadioItems_Genre", "value"),
+    Input("Dropdown_Discipline", "value"),
+    Input("Dropdown_Epreuve", "value"),
+)
+def options_dropdown_category(jeux, genre, discipline, epreuve):
+
+    df = df_jo.copy()
+    df = df[df['Jeux'] == jeux]
+    df = df[df['Genre'] == genre]
+    df = df[df['Discipline'] == discipline]
+    df = df[df['Épreuve'] == epreuve]
+
+    return dt.strptime(df['Date_Début'].min(), '%m-%d-%Y')
 
 
 if __name__=='__main__':
--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.ZEPHYR, dbc.icons.BOOTSTRAP]
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets, title='Finance Dashboard')
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets, title='Jeux Olympiques Dashboard')
 
 
