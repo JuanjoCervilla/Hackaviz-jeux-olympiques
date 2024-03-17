880c257b50c6b7cfb5b6c33feb54a5a4d4bc30bf
Juanjo <jj.cergoma@gmail.com>
2024-03-17 19:01:01 +0100

    server

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ df_jo = pd.read_csv('./data/paris_2024_traite.csv')
 df_restau = pd.read_csv('./data/restaurants_proximité_traite.csv')
 
 
-# app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
+
 external_stylesheets = [dbc.themes.ZEPHYR, dbc.icons.BOOTSTRAP]
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-# app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX])
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets, title='Jeux Olympiques Dashboard')
+
+# app = Dash()
+server = app.server
 
 import plotly.graph_objects as go
 
